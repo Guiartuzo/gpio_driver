58837807ea6fe232f9533f1a4f082904f2d3f223
Guilherme Martim <guilherme.artuzo@hotmail.com>
2026-02-23 07:11:30 -0300

    Include python blink script
    Signed-off-by: Guilherme Martim <guilherme.artuzo@hotmail.com>

--- blink_led.py
+++ blink_led.py
@@ -1,18 +1,16 @@
 import time
 
-
-def blink_led(interval):
-    print("blink_led() called")
-    try:
-        with open('/proc/lll-gpio' , 'w', encoding='utf-8') as file:
-            while(true):
-                print("blinking led...")
-                file.write("21,1")
-                time.sleep(interval)
-                file.write("21,0")
-    except Exception as e:
-        print("Error: {0}".format(e))
-
+def blink_led(pin, state): 
+    try: 
+        with open('/proc/lll-gpio' , 'w', encoding='utf-8') as file: 
+            file.write("{0},{1}".format(pin,state)) 
+    except Exception as e: 
+        print("Error: {0}".format(e)) 
+ 
 if __name__ == "__main__":
     print("blink script started")
-    blink_led(0.5)
+    while True:
+        blink_led(21, 0)
+        time.sleep(0.5)
+        blink_led(21, 1)
+        time.sleep(0.5)
